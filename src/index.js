732398fe48981a7905600090d8a358ae73ec0495
Bichlienntbl2706 <bichlien.27062002@gmail.com>
2024-01-06 21:57:03 +0700

    Static file & SCSS

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const handlebars = require("express-handlebars").engine;
 const app = express();
 const port = 3000;
-//HTTP logger
-app.use(morgan('combined'));
-//template engine
-app.engine('hbs', handlebars({
-    extname: '.hbs'
-})); //
-app.set('view engine', 'hbs'); //đặt cho ứng dụng hiện tại sử dụng engine
-app.set('views', path.join(__dirname, 'resources/views'));
 
-console.log(path.join(__dirname, 'resources/views'))
+app.use(express.static(path.join(__dirname, 'public'))); //kiểm tra bằng phương thức static 
+
+//HTTP logger
+app.use(morgan("combined"));
+//template engine
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs", //đổi tên .handlebars thành .hbs
+  })
+); //
+app.set("view engine", "hbs"); //đặt cho ứng dụng hiện tại sử dụng engine
+app.set("views", path.join(__dirname, "resources/views")); //cấu trúc của engine phải dẫn tới đúng đường link chưa folders views
 
 app.get("/", (req, res) => {
-  res.render('home');
+  res.render("home");
 });
 
 app.get("/news", (req, res) => {
-  res.render('news');
+  res.render("news");//render ra 1 trang có tên là news(yêu cầu phải có file tên là news.hbs)
 });
 
 app.listen(port, () => {
