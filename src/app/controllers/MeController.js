4ddea77819f7cd991280d30673b667e2a49125cd
Bichlienntbl2706 <bichlien.27062002@gmail.com>
2024-01-16 21:01:49 +0700

    middleware

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -3,7 +3,16 @@ const { mutipleMongooseToObject } = require('../../util/mongoose')
 class MeController {
   //[GET] /me/stored/courses
   storedCourses(req, res, next) {
-      Promise.all([Course.find({}), Course.countWithDeleted({ deleted: true })])
+      let courseQuery = Course.find({});
+
+      if(req.query.hasOwnProperty('_sort')){
+          courseQuery = courseQuery.sort({
+            [req.query.column]: req.query.type
+          })
+      }
+
+
+      Promise.all([courseQuery, Course.countWithDeleted({ deleted: true })])
         .then(([courses, deletedCount]) =>
           res.render("me/stored-courses", {
             courses: mutipleMongooseToObject(courses),
--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const port = 3000
 const db = require('./config/db/index')
 
 
+const SortMiddleware = require('./app/middlewares/SortMiddleware')
+
 //Connect to DB
 db.connect()
 
@@ -23,6 +25,8 @@ app.use(express.json())
 
 app.use(methodOverride('_method'))
 
+app.use(SortMiddleware)
+
 //HTTP logger
 app.use(morgan('combined'))
 
@@ -34,6 +38,24 @@ app.engine(
     helpers: {
       //hành động ngoài của người dùng
       sum: (a, b) => a + b,
+      sortable: (field, sort)=>{
+        const sortType = field === sort.column ? sort.type : 'default'
+
+        const icons = {
+          default: 'oi oi-elevator',
+          asc: 'oi oi-sort-ascending',
+          desc: 'oi oi-sort-descending'
+        }
+        const types = {
+          default: 'desc',
+          asc: 'desc',
+          desc: 'asc'
+        }
+
+        const icon = icons[sortType];
+        const type = types[sortType];
+          return `<a href="?_sort&column=${field}&type=${type}"><span class="${icon}"></span></a>`
+      }
     },
   })
 )
